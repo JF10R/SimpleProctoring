e427c8c03a4b0af6d44fbcf65c535775fa7deccb
Jhean-François Fournier-Noël <jfnoel@jfnoel.com>
2023-10-03 23:04:26 +0000

    Structure cleanup, src folder creation, basic index.php style

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SimpleProctoring</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-5 mb-4 text-center">SimpleProctoring</h1>
+        <p class="lead text-center">SimpleProctoring is a browser-based solution to monitor students during online exams.</p>
+        <p class="text-center">It is a simple and easy to use solution that can be used by any educational institution.</p>
+        <p class="text-center">The app has few dependencies: a web browser, a webcam, and a microphone.</p>
+        <div class="text-center">
+            <button class="btn btn-primary">Login with Azure SSO</button>
+        </div>
+    </div>
